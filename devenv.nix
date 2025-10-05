69f3452439347f6cd37c3e43b8ad0b139b1ebe5f
vetsin <131726+vetsin@users.noreply.github.com>
2025-10-04 19:28:59 -0700

    more stuff

--- devenv.nix
+++ devenv.nix
@@ -5,18 +5,24 @@
   languages.ruby.enable = true;
   languages.ruby.bundler.enable = true;
 
-  scripts.jekyll-serve.exec = ''
-    bundle exec jekyll serve --force_polling
-  '';
+#  scripts.jekyll-serve.exec = ''
+#    bundle exec jekyll serve --force_polling
+#  '';
 
-  scripts.jekyll-build.exec = ''
-    bundle exec jekyll build --future
-  '';
+#  scripts.jekyll-build.exec = ''
+#    bundle exec jekyll build --future
+#  '';
 
    tasks = {
       "jekyll:setup".exec = "bundle install";
       "devenv:enterShell".after = [ "jekyll:setup" ];
    };
+
+  processes = {
+    server = {
+      exec = "bundle exec jekyll serve";
+    };
+  };
 
   # git-hooks.hooks.shellcheck.enable = true;
 }
